7e0d6b762902e25fb4196ad24963286a5119d53f
Hassan Helbawi <hassanhelbawi@MacBook-Pro-de-Hassan.local>
2023-02-23 15:06:16 +0100

    updated plugins

--- config/plugins.js
+++ config/plugins.js
@@ -2,14 +2,29 @@ module.exports = ({ env }) => ({
     'import-export-entries': {
         enabled: true,
     },
-    "vercel-deploy": {
-        enabled: true,
-        config: {
-            deployHook:"https://api.vercel.com/v1/integrations/deploy/prj_Y2OzXvI7jagipjIYJPKvzwhh0Z0v/m2b866vayv",
-            apiToken: "yFJCcz8n5Mr9cNgSFBxjVRb7", 
-            appFilter: "swadin-backend",
-            teamFilter: "",
-            roles: ["strapi-super-admin", "strapi-author"],
-          },
-      },
+    // "vercel-deploy": {
+    //     enabled: true,
+    //     config: {
+    //         deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK ,
+    //         apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN, 
+    //         appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+    //         teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+    //         roles: ["strapi-super-admin", "strapi-author"],
+    //       },
+    //   },
+    //   "netlify-deployments": {
+    //     enabled: true,
+    //     config: {
+    //         accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+    //         sites: [
+    //             {
+    //               name: 'aquamarine-cendol-1702ae',
+    //               id: "e6662ea9-c19f-48c6-aa31-7f16abdf19a9",
+    //               buildHook: "https://api.netlify.com/build_hooks/63f76eafb86c7e0c9e10d1fa",
+    //               branch: 'main' // optional
+    //             }
+    //           ]
+    //       },
+    //      
+    //   },
 });
