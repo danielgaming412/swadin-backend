dd9d76aa03f860e1e591fd0d117cacbbc906cd2e
Hassan Helbawi <hassanhelbawi@MacBook-Pro-de-Hassan.local>
2023-02-23 12:56:26 +0100

    token plugin vercel

--- config/plugin.js
+++ config/plugin.js
@@ -4,5 +4,13 @@ module.exports = ({ env }) => ({
     },
     "vercel-deploy": {
         enabled: true,
+        config: {
+            deployHook:
+              "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+            apiToken: "yFJCcz8n5Mr9cNgSFBxjVRb7",
+            appFilter: "swadin-backend",
+            teamFilter: "",
+            roles: ["strapi-super-admin", "strapi-author"],
+          },
       },
 });
